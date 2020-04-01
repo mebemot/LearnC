b844940d229f6d2b261be817dcf96441311a987f
mebemot <thomas.lepard@gmail.com>
2020-04-01 15:25:15 +0100

    Ex 1-18 (test data)

--- chars_noblanks/test.c
+++ chars_noblanks/test.c
@@ -4,33 +4,27 @@
 
 int mgetline(char s[], int lim);
 
-int main()
-{
-    char line[MAXLINE];
-    int i;
+int main() {
+  char line[MAXLINE];
+  int i;
 
-    while ((i = mgetline(line, MAXLINE)) > 0)
-    {
-        printf("[%s] [%d]\n", line, i);
-    }
+  while ((i = mgetline(line, MAXLINE)) > 0) {
+    printf("[%s] [%d]\n", line, i);
+  }
 
-    return 0;
+  return 0;
 }
 
-/* getline: reads line and returns length*/
-int mgetline(char line[], int lim)
-{
-    int c, i;
+/* getline: reads line and returns length(does some bodged stuff for starting w \n)*/
+int mgetline(char line[], int lim) {
+  int c, i;
 
-    for (i = 0; i < lim && (c = getchar()) != EOF && c != '\n'; ++i)
-    {
-        line[i] = c;
-    }
-    if (c == '\n')
-    {
-        line[i] = c;
-        ++i;
-    }
-    line[i] = '\0';
-    return i;
+  for (i = 0; i < lim && (c = getchar()) != EOF && c != '\n'; ++i) {
+    line[i] = c;
+  }
+  line[i] = '\0';
+  if (c == EOF) {
+    return 0;
+  }
+  return i + 1;
 }
